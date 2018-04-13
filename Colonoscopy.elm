2e1733eb1049eb9bb31c44b3234fdaf4c32a9a4f
Giovanni Lodi <giovanni.lodi42@gmail.com>
2018-04-13 13:02:35 +1000

    Generate HTML output from custom QuestionTree input

--- Colonoscopy.elm
+++ Colonoscopy.elm
@@ -1,14 +1,26 @@
 import Html exposing (..)
 
+type alias Question =
+  { text : String
+  , answers: List String
+  }
+
+type alias QuestionTree =
+  { root: Question }
+
+tree : QuestionTree
+tree =
+  { root =
+    { text = "Question"
+    , answers = ["Answer A", "Answer B", "Answer C"]
+    }
+  }
+
 -- In the future we'll want only the list from JSON generation, and management
 -- to be in Elm, the rest should be in a template file. Plain HTML? Pug?
 main =
-  div [] [
-    h1 [] [text "Colonoscopy"]
-    , h2 [] [text "Question?"]
-    , ul [] [
-      li [] [text "Answer A"]
-      , li [] [text "Answer B"]
-      , li [] [text "Answer C"]
-      ]
-    ]
+  div []
+  [ h1 [] [text "Colonoscopy"]
+  , h2 [] [text tree.root.text]
+  , ul [] (List.map (\s -> li [] [text s]) tree.root.answers)
+  ]
